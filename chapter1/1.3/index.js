d1d2e26e04eccad959f84764d3d0f7ffe33fb107
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-09-21 23:22:26 +0100

    implement 1.3 solution

--- chapter1/1.3/index.js
+++ chapter1/1.3/index.js
@@ -1,4 +1,24 @@
-// Write a method to replace all spaces in a string with ‘%20’. You may assume that the string has sufficient space at the end of the string to hold the additional characters, and that you are given the “true” length of the string. (Note: if implementing in Java, please use a character array so that you can perform this operation in place.)
-// Example:
-// Input: "Mr John Smith            ", 13
-// Output: "Mr%20John%20Smith"
+// Write a method to replace all spaces in a string with ‘%20’.
+// You may assume that the string has sufficient space at the end of the string
+// to hold the additional characters, and that you are given the “true” length of the string.
+
+module.exports = function(text) {
+  text = text.split('');
+
+  for (var i in text) {
+    var current = text[i];
+    var next = text.hasOwnProperty(parseInt(i) + 1)
+     ? text[parseInt(i) + 1] : ' ';
+    
+    if (current === ' ') {
+      if (next === ' ') {
+        delete text[i];
+      }
+      else {
+        text[i] = '%20';
+      }
+    }
+  }
+
+  return text.join('');
+};
--- chapter1/1.3/indexSpec.js
+++ chapter1/1.3/indexSpec.js
@@ -1,3 +1,7 @@
 describe("Chapter 1.3", function() {
   var solution = require('./index');
+  
+  it("replace all spaces in a string with ‘%20’", function() {
+    expect(solution('Mr John Smith            ')).toBe("Mr%20John%20Smith");
+  });
 });
