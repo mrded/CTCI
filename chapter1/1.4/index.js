8d354f94b6ed0174c1f59f0cfc9a3e149fc2eaa1
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-10-01 22:15:00 +0100

    implement 1.4

--- chapter1/1.4/index.js
+++ chapter1/1.4/index.js
@@ -1,1 +1,29 @@
-// Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+// Given a string, write a function to check if it is a permutation of a palindrome.
+// A palindrome is a word or phrase that is the same forwards and backwards.
+// A permutation is a rearrangement of letters.
+// The palindrome does not need to be limited to just dictionary words.
+
+module.exports = function(text) {
+  var chars = [];
+
+  // Remove spaces
+  text = text.replace(/ /, '');
+
+  // Convert to lower case
+  text = text.toLowerCase();
+
+  for (var i in text) {
+    // Collect unique chars.
+    var char = text[i];
+    var charIndex = chars.indexOf(char);
+
+    if (charIndex === -1) {
+      chars.push(char); 
+    }
+    else {
+      chars.splice(charIndex, 1);
+    }
+  }
+
+  return chars.length === 1 || chars.length === 0;
+};
--- chapter1/1.4/indexSpec.js
+++ chapter1/1.4/indexSpec.js
@@ -1,3 +1,19 @@
 describe("Chapter 1.4", function() {
   var solution = require('./index');
+
+  it("'Tact Coa' must return true", function() {
+    expect(solution('Tact Coa')).toBe(true);
+  });
+
+  it("'Tactcoa' must return true", function() {
+    expect(solution('Tactcoa')).toBe(true);
+  });
+
+  it("'Cact Coa' must return false", function() {
+    expect(solution('Cact Coa')).toBe(false);
+  });
+
+  it("'Tact Co' must return false", function() {
+    expect(solution('Tact Co')).toBe(false);
+  });
 });
