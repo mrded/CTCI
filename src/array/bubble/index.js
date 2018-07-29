a10dd75a1dc2235c505a95328dbd4895e5ca2db1
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-07-29 14:43:28 +0100

    Optimise bubble sort.

--- src/array/bubble/index.js
+++ src/array/bubble/index.js
@@ -5,6 +5,8 @@ module.exports = function(array) {
   let iterations = 0;
 
   for (let i = 0; i < array.length; i++) { // O(n) 
+    let swaps = 0;
+
     // No needs to check last element, because it will be max already.
     for (let j = 0; j+i < array.length; j++) { // O(n-i); i = (n-1) + (n-2) + (n-n)
       iterations++;
@@ -13,13 +15,20 @@ module.exports = function(array) {
       const y = output[j+1];
 
       if (x > y) {
+        swaps++;
         output[j] = y;
         output[j+1] = x;
       }
     }
+
+    // If no swaps - return, because it's already sorted.
+    if (swaps === 0) {
+      console.log(array, 'iterations:', iterations);
+      return output;
+    }
   }
 
-  // console.log(array, 'iterations:', iterations);
+  console.log(array, 'iterations:', iterations);
 
   return output;
 }
