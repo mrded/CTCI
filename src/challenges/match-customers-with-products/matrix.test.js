d125c86292d2d53dca6eeb002199d2e20c5002e7
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-09-02 16:10:05 +0100

    Add tests.

--- src/challenges/match-customers-with-products/matrix.test.js
+++ src/challenges/match-customers-with-products/matrix.test.js
@@ -72,66 +72,101 @@ describe('Matrix', function() {
   });
 
   describe('findMaxCombination', () => {
-    describe('3x3', () => {
-      it('variant 1', () => {
-        const matrix = new Matrix(3, 3);
+    it('variant 1', () => {
+      const matrix = new Matrix(3, 3);
 
-        matrix.matrix = [
-          [1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]
-        ];
+      matrix.matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ];
 
-        expect(matrix.findMaxCombination()).toBe(9+5+1);
-      });
-    })
-
-    describe('2x3', () => {
-      it('variant 1', () => {
-        const matrix = new Matrix(2, 3);
-
-        matrix.matrix = [
-          [1, 2, 3],
-          [4, 5, 6],
-        ];
-
-        expect(matrix.findMaxCombination()).toBe(6+2);
-      });
-
-      it('variant 2', () => {
-        const matrix = new Matrix(2, 3);
-
-        matrix.matrix = [
-          [5, 3, 1],
-          [6, 4, 2],
-        ];
-
-        expect(matrix.findMaxCombination()).toBe(6+3);
-      });
+      expect(matrix.findMaxCombination()).toBe(9+5+1);
     });
 
-    describe('3x2', () => {
-      it('variant 1', () => {
-        const matrix = new Matrix(3, 2);
+    it('variant 2', () => {
+      const matrix = new Matrix(2, 3);
 
-        matrix.matrix = [
-          [1, 2],
-          [3, 4],
-          [5, 6]
-        ];
+      matrix.matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+      ];
 
-        expect(matrix.findMaxCombination()).toBe(6+3);
-      });
+      expect(matrix.findMaxCombination()).toBe(6+2);
     });
 
-    describe('1x1', () => {
-      it('variant 1', () => {
-        const matrix = new Matrix(1, 1);
+    it('variant 3', () => {
+      const matrix = new Matrix(2, 3);
 
-        matrix.matrix = [ [42] ];
+      matrix.matrix = [
+        [5, 3, 1],
+        [6, 4, 2],
+      ];
 
-        expect(matrix.findMaxCombination()).toBe(42);
-      });
+      expect(matrix.findMaxCombination()).toBe(6+3);
+    });
+
+    it('variant 4', () => {
+      const matrix = new Matrix(3, 2);
+
+      matrix.matrix = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+      ];
+
+      expect(matrix.findMaxCombination()).toBe(6+3);
+    });
+
+    it('variant 5', () => {
+      const matrix = new Matrix(1, 1);
+
+      matrix.matrix = [ [42] ];
+
+      expect(matrix.findMaxCombination()).toBe(42);
+    });
+
+    it('variant 6', () => {
+      const matrix = new Matrix(3, 3);
+
+      matrix.matrix = [
+        [ 6, 7, 6 ],
+        [ 4.5, 6, 6.75 ], 
+        [ 6, 5, 9 ]
+      ];
+
+      expect(matrix.findMaxCombination()).toBe(21);
+    });
+
+    it('variant 7', () => {
+      const matrix = new Matrix(6, 6);
+
+      matrix.matrix = [
+        [ 9, 13.5, 8, 8, 13.5, 8 ],
+        [ 6, 6, 12, 8, 6, 8 ],
+        [ 22.5, 22.5, 14, 14, 22.5, 14 ],
+        [ 9, 9, 8, 8, 9, 8 ],
+        [ 7.5, 7.5, 12, 8, 7.5, 8 ],
+        [ 18, 18, 10, 10, 18, 10 ]
+      ];
+
+      expect(matrix.findMaxCombination()).toBe(83.50);
+    });
+
+    it('variant 8', () => {
+      const matrix = new Matrix(7, 6);
+
+      matrix.matrix = [
+        [ 13.5, 13.5, 13.5, 4.5, 13.5, 13.5 ],
+        [ 4.5, 4.5, 4.5, 4, 4.5, 4.5 ],
+        [ 10.5, 10.5, 10.5, 10, 10.5, 10.5 ],
+        [ 11.25, 11.25, 11.25, 2, 11.25, 11.25 ],
+        [ 7.5, 11.25, 11.25, 13.5, 7.5, 11.25 ],
+        [ 7.5, 7.5, 7.5, 8, 7.5, 7.5 ],
+        [ 13.5, 13.5, 13.5, 9, 13.5, 13.5 ]
+      ];
+
+      expect(matrix.findMaxCombination()).toBe(71.25);
     });
   });
 });
