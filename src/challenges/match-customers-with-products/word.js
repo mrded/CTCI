2b3929ad68936a5a2c9f984e2ee0ee867d2da0e2
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-09-02 17:29:32 +0100

    Refactor into ES6 class.

--- src/challenges/match-customers-with-products/word.js
+++ src/challenges/match-customers-with-products/word.js
@@ -1,26 +1,33 @@
-function Word(word) {
-  this.word = word;
-
-  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-  const consonants = [
-    'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
-    'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
-  ];
-
-  this.countVowels = () => this.count(vowels);
-  this.countConsonants = () => this.count(consonants);
-  this.countLetters = () => this.count(vowels.concat(consonants))
-}
-
-Word.prototype.count = function(dict) {
-  let count = 0;
-
-  // Count letters mach dictionary.
-  for (let letter of this.word.toLowerCase()) {
-    if (dict.indexOf(letter) !== -1) count++;   
+class Word {
+  constructor(word) {
+    this.word = word;
   }
 
-  return count;
+  get vowels() {
+    return this.count(['a', 'e', 'i', 'o', 'u', 'y']);
+  } 
+
+  get consonants() {
+    return this.count([
+      'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 
+      'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
+    ]);
+  }
+
+  get letters() {
+    return this.vowels + this.consonants;
+  }
+
+  count(dict) {
+    let count = 0;
+
+    // Count letters mach dictionary.
+    for (let letter of this.word.toLowerCase()) {
+      if (dict.indexOf(letter) !== -1) count++;   
+    }
+
+    return count;
+  }
 }
 
 module.exports = Word;
