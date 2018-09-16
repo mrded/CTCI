6c88e764edbdbe7da171d9eca85322837ca8ddc7
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-09-16 20:45:22 +0100

    Refactor

--- data-structures/binary-search-tree/index.js
+++ data-structures/binary-search-tree/index.js
@@ -1,41 +1,6 @@
 const Node = require('binary-node');
 
-function _delete(node, data) {
-  // 0. find a node
-  if (data < node.data) {
-    node.left = _delete(node.left, data);
-  }
-  else if (data > node.data) {
-    node.right = _delete(node.right, data);
-  }
-  else if (data === node.data) {
-    // 1. no children - delete relation
-    if (!node.left && !node.right) {
-      node = null;
-    }
-
-    // 2. one child - move relation to a child
-    else if (node.left && !node.right) {
-      node = node.left;
-    }
-    else if (!node.left && node.right) {
-      node = node.right;
-    }
-
-    // 3. two children - (left.max || right.min) is root
-    else {
-      // Two options: 
-      //  - find max value from the left and move it to node.data
-      //  - find min value from the right and move it to node.data
-      const min = node.right.min();
-
-      node.data = min.data;
-      node.right = _delete(node.right, min.data);
-    }
-  }
-
-  return node;
-}
+const _delete = require('./delete.js');
 
 class BST extends Node {
   insert(data) {
